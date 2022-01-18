a62ea2ae188c42692ab425a440cc3409cbd69373
amoldy <43250812+amoldy@users.noreply.github.com>
2022-01-18 17:47:43 -0500

    update url

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://practical-almeida-9e9580.netlify.com`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
